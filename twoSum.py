0c38ec8ae932feb6d416e71cd50169d7d0a141f7
changh <changh@soyuan.com.cn>
2019-03-20 09:40:29 +0800

    两数之和

--- twoSum.py
+++ twoSum.py
@@ -1,6 +1,11 @@
 class Solution:
     def twoSum(self, nums, target):
-
+        """
+        两数之和
+        :param nums: 整数列表
+        :param target: 目标值
+        :return: 符合条件下表列表
+        """
         hash_map = {}
         for index, num in enumerate(nums):
             another_num = target - num
